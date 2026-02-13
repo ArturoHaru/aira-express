7cd5b0dc9b2e8b81a7571177670c605c834d5178
Arturo Cecora <arturocecora@proton.me>
2026-02-13 09:38:07 +0100

    Create speaches.route.ts

---
+++ routes/speaches.route.ts
@@ -0,0 +1,12 @@
+import {
+  getAudioSynthesis,
+  getTranscription,
+} from "../controllers/speaches.controller";
+import { Router } from "express";
+import multer from "multer";
+
+const upload = multer({ storage: multer.memoryStorage() });
+export const router = Router();
+
+router.post("/speaches/transcribe", upload.single("file"), getTranscription);
+router.post("/speaches/sinthesize", getAudioSynthesis);
